cd2cb03bbe03e8ec0d5b622f5ded9588784dacfd
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-04 11:48:14 +0600

    Explored array types

--- app.ts
+++ app.ts
@@ -9,6 +9,14 @@
 const person = {
   name: "Ashikur Rahman",
   age: 19,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
