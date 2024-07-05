215470e55b44133c6440ead75479eba4eaf91fb3
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-06 05:24:50 +0600

    Explored tupils

--- app.ts
+++ app.ts
@@ -6,11 +6,20 @@
 //   age: 19,
 // };
 
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Ashikur Rahman",
   age: 19,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "Author"],
 };
+
+// person.role.push("admin");
+// person.role[1] = 10;
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
