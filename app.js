8c87e23a0494036ce17496bc4c4ab01cb96f26a6
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-06 07:01:26 +0600

    Explored watch mode

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = "Ashik";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("An error occurred", 500);
+// let userInput: unknown;
+// let userName: string;
+// userInput = 5;
+// userInput = "Ashik";
+// if (typeof userInput === "string") {
+//   userName = userInput;
+// }
+// function generateError(message: string, code: number): never {
+//   throw { message: message, errorCode: code };
+// }
+// generateError("An error occurred", 500);
+var userName = "Ashikur Rahman Ashik";
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,15 +1,18 @@
-let userInput: unknown;
-let userName: string;
+// let userInput: unknown;
+// let userName: string;
 
-userInput = 5;
-userInput = "Ashik";
+// userInput = 5;
+// userInput = "Ashik";
 
-if (typeof userInput === "string") {
-  userName = userInput;
-}
+// if (typeof userInput === "string") {
+//   userName = userInput;
+// }
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
+// function generateError(message: string, code: number): never {
+//   throw { message: message, errorCode: code };
+// }
 
-generateError("An error occurred", 500);
+// generateError("An error occurred", 500);
+
+const userName = "Ashikur Rahman Ashik";
+console.log(userName);
