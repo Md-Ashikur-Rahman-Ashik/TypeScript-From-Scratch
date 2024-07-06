1a92fc48c5ab61dee08dbafa1096b99ff62b9ac0
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-07-06 06:08:01 +0600

    Explored literal types

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    //   if (resultConversion === "as-number") {
+    //     return +result;
+    //   } else {
+    //     return result.toString();
+    //   }
 }
-var combinedNames = combine("Ashikur", " Rahman");
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Ashikur", " Rahman", "as-text");
 console.log(combinedNames);
