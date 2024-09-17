f0e0244357699e5b68525e9ad18c522a1dcc755b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-09-17 11:13:36 +0600

    Revised basic types

--- helloWorld/types.js
+++ helloWorld/types.js
@@ -1,1 +1,16 @@
-var n = 1;
+// let n: Number = 1;
+// n = "Robb"
+var isWinter = false;
+// isWinter = 123;
+var count = 5;
+var userName = "Bran";
+var names = ["John", "Rickon"];
+var Starks;
+(function (Starks) {
+    Starks[Starks["John"] = 0] = "John";
+    Starks[Starks["Bran"] = 1] = "Bran";
+})(Starks || (Starks = {}));
+var cat = Starks.John;
+function getName() {
+    return "John";
+}
--- helloWorld/types.ts
+++ helloWorld/types.ts
@@ -1,3 +1,27 @@
-let n: Number = 1;
+// let n: Number = 1;
 
 // n = "Robb"
+
+let isWinter: boolean = false;
+
+// isWinter = 123;
+
+let count: Number = 5;
+let userName: String = "Bran";
+
+let names: string[] = ["John", "Rickon"];
+
+enum Starks {
+  John,
+  Bran,
+}
+
+let cat: Starks = Starks.John;
+
+function getName(): String {
+  return "John";
+}
+
+function anotherName(): void {
+  console.log("Winter is coming");
+}
