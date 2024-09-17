6ec5198be14e5128fca7ece842fa472005a1364a
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-09-17 11:24:07 +0600

    Revised interfaces

--- helloWorld/types.js
+++ helloWorld/types.js
@@ -1,16 +1,22 @@
 // let n: Number = 1;
 // n = "Robb"
-var isWinter = false;
+// let isWinter: boolean = false;
 // isWinter = 123;
-var count = 5;
-var userName = "Bran";
-var names = ["John", "Rickon"];
-var Starks;
-(function (Starks) {
-    Starks[Starks["John"] = 0] = "John";
-    Starks[Starks["Bran"] = 1] = "Bran";
-})(Starks || (Starks = {}));
-var cat = Starks.John;
-function getName() {
-    return "John";
+// let count: Number = 5;
+// let userName: String = "Bran";
+// let names: string[] = ["John", "Rickon"];
+// enum Starks {
+//   John,
+//   Bran,
+// }
+// let cat: Starks = Starks.John;
+// function getName(): String {
+//   return "John";
+// }
+// function anotherName(): void {
+//   console.log("Winter is coming");
+// }
+function printName(stark) {
+    console.log(stark.name);
 }
+printName({ name: "Ashik" });
--- helloWorld/types.ts
+++ helloWorld/types.ts
@@ -2,26 +2,38 @@
 
 // n = "Robb"
 
-let isWinter: boolean = false;
+// let isWinter: boolean = false;
 
 // isWinter = 123;
 
-let count: Number = 5;
-let userName: String = "Bran";
+// let count: Number = 5;
+// let userName: String = "Bran";
 
-let names: string[] = ["John", "Rickon"];
+// let names: string[] = ["John", "Rickon"];
 
-enum Starks {
-  John,
-  Bran,
+// enum Starks {
+//   John,
+//   Bran,
+// }
+
+// let cat: Starks = Starks.John;
+
+// function getName(): String {
+//   return "John";
+// }
+
+// function anotherName(): void {
+//   console.log("Winter is coming");
+// }
+
+interface Stark {
+  name: String;
+  age?: Number;
 }
 
-let cat: Starks = Starks.John;
-
-function getName(): String {
-  return "John";
+function printName(stark: Stark) {
+  console.log(stark.name);
 }
 
-function anotherName(): void {
-  console.log("Winter is coming");
-}
+printName({ name: "Ashik" });
+// printName({ label: "Montu" });
